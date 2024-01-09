f0c136e0c8b8635614e3034cfc40e729e8445982
lake <102535481@qq.com>
2024-01-09 18:10:53 +0800

    优化

--- api/src/error.rs
+++ api/src/error.rs
@@ -1,14 +1,13 @@
 use actix_web::{HttpResponse, ResponseError};
-use migration::DbErr;
 use crate::response::resp_fail;
 
 #[derive(thiserror::Error, Debug)]
 pub enum MyError {
     #[error("{0}")]
     Msg(String),
-    #[error("DbError: {0}")]
-    DbError(#[from] DbErr),
-    #[error("TokenError: {0}")]
+    #[error("db_error: {0}")]
+    DbError(#[from] migration::DbErr),
+    #[error("token_error: {0}")]
     TokenError(#[from] jsonwebtoken::errors::Error),
 }
 
