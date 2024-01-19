51fe6cbc0544249a5a23c5c7d7bdd88abc405de9
lake <102535481@qq.com>
2024-01-19 18:22:25 +0800

    init

--- api/src/view/pay.rs
+++ api/src/view/pay.rs
@@ -1,6 +1,9 @@
+use actix_web::http::StatusCode;
 use actix_web::web::{scope, Bytes, Data, Json, Path, ServiceConfig};
 use actix_web::{post, HttpRequest, HttpResponse};
-use tracing::{debug, instrument};
+use serde::{Deserialize, Serialize};
+use service::error::ServiceError;
+use tracing::{debug, error, instrument};
 use wechat_pay_rust_sdk::model::{H5Params, H5SceneInfo, WechatPayDecodeData, WechatPayNotify};
 use wechat_pay_rust_sdk::pay::{PayNotifyTrait, WechatPay};
 
@@ -8,9 +11,9 @@ use crate::base::error::ApiError;
 use crate::base::response::resp_ok_empty;
 use crate::base::state::AppState;
 
-#[post("/weixin/notify")]
+#[post("/wechat/notify")]
 #[instrument(skip(state))]
-async fn weixin_notify(
+async fn wechat_notify(
     state: Data<AppState>,
     data: Json<WechatPayNotify>,
 ) -> Result<HttpResponse, ApiError> {
@@ -23,33 +26,67 @@ async fn weixin_notify(
         .ok_or(ApiError::msg("associated_data is none".to_string()))?;
     dotenvy::dotenv().ok();
     let wechat_pay = WechatPay::from_env();
-    let result: WechatPayDecodeData = wechat_pay
+    let result = wechat_pay
         .decrypt_paydata(
             ciphertext,      //加密数据
             nonce,           //随机串
             associated_data, //关联数据
         )
-        .await
         .map_err(|e| ApiError::msg(e.to_string()))?;
     debug!("pay notify decrypt result: {:?}", result);
-    service::pay::change_payed_status(&state.conn, result.out_trade_no).await?;
-    Ok(HttpResponse::Ok().json(serde_json::json!({
-        "code":"SUCCESS",
-        "message":"成功",
-    })))
+    match service::pay::change_payed_status(&state.conn, result.out_trade_no).await {
+        Ok(_) => Ok(HttpResponse::Ok().status(StatusCode::NO_CONTENT).finish()),
+        Err(e) => {
+            error!("change_payed_status error: {:?}", e);
+            let status500 = StatusCode::INTERNAL_SERVER_ERROR;
+            Ok(HttpResponse::Ok().status(status500).finish())
+        }
+    }
 }
-#[post("/weixin/{id}")]
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct PayParams {
+    money: u32,
+    time: u64,
+    sign: String,
+}
+
+#[post("/wechat/order")]
 #[instrument(skip(state))]
-async fn pay(state: Data<AppState>, _id: Path<String>) -> Result<HttpResponse, ApiError> {
-    let wechat_pay = WechatPay::from_env();
-    let _conn = &state.conn;
-    let pay_params = H5Params::new(
-        "测试支付1分",
-        "1243243",
-        1.into(),
-        H5SceneInfo::new("8.210.234.214", "rust收钱", "https://crates.io"),
-    );
-    wechat_pay.h5_pay(pay_params).await.unwrap();
+async fn wechat_pay_order(
+    state: Data<AppState>,
+    data: Json<PayParams>,
+) -> Result<HttpResponse, ApiError> {
+    let data = data.into_inner();
+
+    let mut maps = vec![
+        ("money", data.money.to_string()),
+        ("time", data.time.to_string()),
+    ];
+    maps.sort_by(|a, b| a.0.cmp(&b.0));
+    let sign_str = maps
+        .into_iter()
+        .map(|(k, v)| format!("{}={}", k, v))
+        .collect::<Vec<String>>()
+        .join("\n");
+    let sign = util::crypto::md5(sign_str);
+    if sign != data.sign {
+        return Err(ApiError::msg("签名验证失败".to_string()));
+    }
+    let time = chrono::Local::now().timestamp();
+    // 5分钟有效期
+    if time - data.time as i64 > 5 * 60 {
+        return Err(ApiError::msg("定单失效，请重新创建".to_string()));
+    }
+    // let wechat_pay = WechatPay::from_env();
+    // let _conn = &state.conn;
+    // let pay_params = H5Params::new(
+    //     "测试支付1分",
+    //     "1243243",
+    //     1.into(),
+    //     H5SceneInfo::new("8.210.234.214", "rust收钱", "https://crates.io"),
+    // );
+    // wechat_pay.h5_pay(pay_params).await.unwrap();
 
     Ok(HttpResponse::Ok().json(resp_ok_empty()))
     //     .await
@@ -60,5 +97,9 @@ async fn pay(state: Data<AppState>, _id: Path<String>) -> Result<HttpResponse, A
 }
 
 pub fn configure(cfg: &mut ServiceConfig) {
-    cfg.service(scope("/pay").service(pay));
+    cfg.service(
+        scope("/pay")
+            .service(wechat_notify)
+            .service(wechat_pay_order),
+    );
 }
