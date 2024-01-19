b7aed942f6ddc4dc72d1de3ddb575140626d762b
lake <102535481@qq.com>
2024-01-19 14:14:32 +0800

    优化测试

--- service/src/pay.rs
+++ service/src/pay.rs
@@ -48,6 +48,7 @@ pub async fn change_payed_status(
             let coin = info.coin;
             let mut acive_model: PayActiveModel = info.into();
             acive_model.payed = Set(true);
+            acive_model.payed_time = Set(Some(util::time::now()));
             Pay::update(acive_model).exec(&tx).await?;
             super::pay_record::user_coin_change(
                 &tx,
@@ -65,11 +66,13 @@ pub async fn change_payed_status(
 
 #[cfg(test)]
 mod tests {
+    use crate::error::ServiceError;
     use sea_orm::Database;
     use tracing::debug;
 
+    /// 测试创建订单
     #[tokio::test]
-    async fn test_change_payed_status() {
+    async fn test_change_payed_status() -> Result<(), ServiceError> {
         dotenvy::dotenv().ok();
         tracing_subscriber::fmt::init();
         let db_url = std::env::var("DATABASE_URL").unwrap();
@@ -80,6 +83,37 @@ mod tests {
             .await
             .unwrap();
         debug!("pay_id: {}", pay_id);
+        super::change_payed_status(&conn, pay_id, 1).await
+    }
+
+    /// 测试订单不存在
+    #[tokio::test]
+    #[should_panic(expected = "订单不存在")]
+    async fn test_change_payed_status_not_found() {
+        dotenvy::dotenv().ok();
+        tracing_subscriber::fmt::init();
+        let db_url = std::env::var("DATABASE_URL").unwrap();
+        let conn = Database::connect(db_url)
+            .await
+            .expect("Cannot connect to database");
+        super::change_payed_status(&conn, -1, 1).await.unwrap();
+    }
+
+    /// 测试重复支付
+    #[tokio::test]
+    #[should_panic(expected = "订单已支付，请不要重复支付")]
+    async fn test_change_payed_status_repeat_pay() {
+        dotenvy::dotenv().ok();
+        tracing_subscriber::fmt::init();
+        let db_url = std::env::var("DATABASE_URL").unwrap();
+        let conn = Database::connect(db_url)
+            .await
+            .expect("Cannot connect to database");
+        let pay_id = super::create_pay(&conn, 1, super::PayPlatform::Wechat, 1, 1)
+            .await
+            .unwrap();
+        debug!("pay_id: {}", pay_id);
+        super::change_payed_status(&conn, pay_id, 1).await.unwrap();
         super::change_payed_status(&conn, pay_id, 1).await.unwrap();
     }
 }
